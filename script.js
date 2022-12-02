d10757cf275778d3514706753c1fb00ea3ba7b5e
cyberwerkstatt <seslija.sasha@gmail.com>
2022-12-02 23:02:08 +0100

    more styling

--- script.js
+++ script.js
@@ -8,20 +8,23 @@ async function loadPokemon(){
     let response = await fetch(url);
     let responseAsJSON = await response.json();
     let pokePic = responseAsJSON["sprites"]["other"]["official-artwork"]["front_default"]
-    
+    let name = responseAsJSON["name"];
     let pokeContainer = document.getElementById("container");
     pokeContainer.innerHTML = `
-        <div><h1>${responseAsJSON["name"]}</h1></div>
+        <div class="pokeName">
+            <h2>Name: ${name.toUpperCase()}</h2>
+        </div>
         <img src="${pokePic}">
     `;
 
     loadStats(responseAsJSON);
-    pokemon.value = "";
+    document.getElementById("input").value = "";
 }
 
 function loadStats(pokemon_stats){
     let type = pokemon_stats["types"][0]["type"]["name"]
+    let special_attack = "";
     let stats_container = document.getElementById("stats");
-    stats_container.innerHTML += type;
+    stats_container.innerHTML = `<p>Type: ${type}</p>`;
     console.log(pokemon_stats)
 }
