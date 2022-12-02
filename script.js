d494afb4fe904d49b5724acd321b3bcd42a660cc
cyberwerkstatt <seslija.sasha@gmail.com>
2022-12-03 00:34:02 +0100

    updated functions

--- script.js
+++ script.js
@@ -16,16 +16,44 @@ async function loadPokemon(){
         </div>
         <img src="${pokePic}">
     `;
-
     loadStats(responseAsJSON);
     document.getElementById("input").value = "";
 }
 
 function loadStats(pokemon_stats){
+    removeColorClasses();
     let type = pokemon_stats["types"][0]["type"]["name"]
     let stats = pokemon_stats["stats"];
     let stats_container = document.getElementById("stats");
-    stats_container.innerHTML = `<p>Type: ${type}</p>`;
-    console.log(stats)
+    stats_container.innerHTML = `Type: ${type}`;
+    addColorClasses(pokemon_stats);
 
+    console.log(stats)
 }
+
+function addColorClasses(pokemon_stats){
+    stats_container = document.getElementById("stats");
+    type = pokemon_stats["types"][0]["type"]["name"];
+    if(type == "fire"){
+        stats_container.classList.add("red");
+    } else if (type == "water") {
+        stats_container.classList.add("blue")
+    } else if (type == "grass") {
+        stats_container.classList.add("green")
+    } else if (type == "psychic") {
+        stats_container.classList.add("psych")
+    } else if (type == "electric") {
+        stats_container.classList.add("yellow")
+    } else if (type == "rock") {
+        stats_container.classList.add("stone")
+    }
+}
+
+function removeColorClasses(){
+    let colors = ["red", "blue", "yellow", "green", "psych", "stone"];
+    for (let i = 0; i < colors.length; i++) {
+        let color = colors[i];
+        document.getElementById("stats").classList.remove(color);
+    }
+}
+
