787d16bb0acc41a4e17d7eb62899fc5c7a3909a2
cyberwerkstatt <seslija.sasha@gmail.com>
2022-12-02 10:38:26 +0100

    first function added

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// first function created
+
+
+async function loadPokemon(){
+    let pokemon = document.getElementById("input").value;
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    let response = await fetch(url);
+    let responseAsJSON = await response.json();
+    let pokePic = responseAsJSON["sprites"]["other"]["official-artwork"]["front_default"]
+    
+    let pokeContainer = document.getElementById("container");
+    pokeContainer.innerHTML = `
+    <div><h1>${responseAsJSON["name"]}</h1></div>
+    <img src="${pokePic}">
+    `;
+    console.log("Loaded Pokemon:", responseAsJSON);
+}
